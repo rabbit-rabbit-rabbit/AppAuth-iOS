6eab384d05f98a41a39720f484b789afe156a08e
Andrew McLean <andrew@createwithplay.com>
2024-04-24 12:59:33 -0600

    Update catalyst user agent to use webview from formsheet

--- Sources/AppAuth/iOS/OIDExternalUserAgentCatalyst.m
+++ Sources/AppAuth/iOS/OIDExternalUserAgentCatalyst.m
@@ -33,7 +33,132 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface OIDExternalUserAgentCatalyst ()<ASWebAuthenticationPresentationContextProviding>
+@import WebKit;
+@import UIKit;
+
+@protocol OIDWebViewControllerDelegate;
+
+@interface OIDWebViewController: UIViewController {
+
+}
+@property(nonatomic, strong) NSString *redirectScheme;
+@property(nonatomic, strong) NSURL *requestURL;
+@property(nonatomic, assign) id<OIDWebViewControllerDelegate> delegate;
+
+-(void)loadContent;
+
+@end
+
+@protocol OIDWebViewControllerDelegate<NSObject>
+
+-(void)webViewDidFinish:(NSURL *)callbackURL;
+
+-(void)webViewDidClose;
+
+@end
+
+@interface OIDWebViewController () <WKUIDelegate, WKNavigationDelegate> {
+
+}
+@property(nonatomic, strong) WKWebView *webView;
+
+@end
+
+@implementation OIDWebViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+//  self.navigationController.navigationBar.translucent = NO;
+//  self.navigationController.toolbar.translucent = NO;
+
+  [self createLeftBarButtonItem];
+}
+
+-(void)createLeftBarButtonItem
+{
+    if (self.navigationItem.leftBarButtonItem == nil)
+    {
+        UIImage *xIcon = [[UIImage systemImageNamed:@"xmark"]
+                          imageWithTintColor:[UIColor secondaryLabelColor] renderingMode:UIImageRenderingModeAlwaysOriginal];
+        UIBarButtonItem *closeButton = [[UIBarButtonItem alloc]
+                                         initWithImage:xIcon
+                                         style:UIBarButtonItemStylePlain
+                                         target:self
+                                         action:@selector(doClose:)];
+        
+        self.navigationItem.leftBarButtonItem = closeButton;
+    }
+}
+
+-(void)doClose:(id)sender
+{
+   [self dismissViewControllerAnimated:YES completion:^{
+        if ([self.delegate respondsToSelector:@selector(webViewDidClose)]) {
+            [self.delegate webViewDidClose];
+        }
+   }];
+}
+
+-(void)constructView:(CGSize)boundSize
+{
+    if (self.webView == nil) {
+        WKWebViewConfiguration *webViewConfiguration = [[WKWebViewConfiguration alloc] init];
+        webViewConfiguration.allowsInlineMediaPlayback = NO;
+        
+        // Initialize the WKWebView with your WKWebViewConfiguration object.
+        self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, boundSize.width, boundSize.height) configuration:webViewConfiguration];
+        self.webView.navigationDelegate = self;
+        self.webView.UIDelegate = self;
+        self.webView.customUserAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15";
+              
+        self.webView.scrollView.bounces = NO;
+
+        [self.view addSubview:self.webView];
+    }
+}
+
+-(void)resetView:(CGSize)boundSize
+{
+    self.webView.frame = CGRectMake(0, 0, boundSize.width, boundSize.height);
+}
+
+-(void)actualLoadContent
+{
+    [self constructView:self.view.bounds.size];
+    
+      [self resetView:self.view.bounds.size];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.requestURL];
+    [self.webView loadRequest:request];
+}
+
+-(void)loadContent
+{
+    [self performSelector:@selector(actualLoadContent) withObject:nil afterDelay:0.1f];
+}
+
+-(void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    NSString *urlString = navigationAction.request.URL.absoluteString;
+    //NSLog(@"urlString: %@", urlString);
+    if ([urlString hasPrefix:self.redirectScheme]) {
+
+        [self dismissViewControllerAnimated:YES completion:^{
+            if ([self.delegate respondsToSelector:@selector(webViewDidFinish:)]) {
+                [self.delegate webViewDidFinish:navigationAction.request.URL];
+            }
+        }];
+
+        decisionHandler(NO);
+    } else {
+        decisionHandler(YES);
+    }
+}
+
+@end
+
+@interface OIDExternalUserAgentCatalyst ()<ASWebAuthenticationPresentationContextProviding, OIDWebViewControllerDelegate>
 @end
 
 @implementation OIDExternalUserAgentCatalyst {
@@ -42,7 +167,9 @@ NS_ASSUME_NONNULL_BEGIN
 
   BOOL _externalUserAgentFlowInProgress;
   __weak id<OIDExternalUserAgentSession> _session;
-  ASWebAuthenticationSession *_webAuthenticationVC;
+  //ASWebAuthenticationSession *_webAuthenticationVC;
+
+  __weak OIDWebViewController *_safariVC;
 }
 
 - (nullable instancetype)initWithPresentingViewController:
@@ -78,7 +205,8 @@ NS_ASSUME_NONNULL_BEGIN
 
   __weak OIDExternalUserAgentCatalyst *weakSelf = self;
   NSString *redirectScheme = request.redirectScheme;
-  ASWebAuthenticationSession *authenticationVC =
+
+  /*ASWebAuthenticationSession *authenticationVC =
       [[ASWebAuthenticationSession alloc] initWithURL:requestURL
                                     callbackURLScheme:redirectScheme
                                     completionHandler:^(NSURL * _Nullable callbackURL,
@@ -102,7 +230,31 @@ NS_ASSUME_NONNULL_BEGIN
   authenticationVC.presentationContextProvider = self;
   authenticationVC.prefersEphemeralWebBrowserSession = _prefersEphemeralSession;
   _webAuthenticationVC = authenticationVC;
-  openedUserAgent = [authenticationVC start];
+  openedUserAgent = [authenticationVC start];*/
+
+  if (!openedUserAgent && _presentingViewController) {
+      OIDWebViewController *safariVC = [[OIDWebViewController alloc] init];
+      safariVC.redirectScheme = redirectScheme;
+      safariVC.requestURL = requestURL;
+      safariVC.delegate = self;
+      _safariVC = safariVC;
+
+      CGFloat height = [[UIApplication sharedApplication] keyWindow].bounds.size.height;
+      
+      safariVC.preferredContentSize = CGSizeMake(768, height * 0.8);
+
+      UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:safariVC];
+      navigationController.modalInPresentation = YES;
+      navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
+      navigationController.navigationBar.translucent = NO;
+      navigationController.toolbar.translucent = NO;
+    
+      [_presentingViewController presentViewController:navigationController animated:YES completion:^{
+
+          [safariVC loadContent];
+      }];
+      openedUserAgent = YES;
+    }
 
   if (!openedUserAgent) {
     [self cleanUp];
@@ -121,25 +273,67 @@ NS_ASSUME_NONNULL_BEGIN
     return;
   }
   
-  ASWebAuthenticationSession *webAuthenticationVC = _webAuthenticationVC;
+  //ASWebAuthenticationSession *webAuthenticationVC = _webAuthenticationVC;
   
   [self cleanUp];
   
-  if (webAuthenticationVC) {
+  /*if (webAuthenticationVC) {
     // dismiss the ASWebAuthenticationSession
     [webAuthenticationVC cancel];
     if (completion) completion();
   } else {
     if (completion) completion();
-  }
+  }*/
+
+  if (completion) completion();
 }
 
 - (void)cleanUp {
   // The weak reference to |_session| is set to nil to avoid accidentally using
   // it while not in an authorization flow.
-  _webAuthenticationVC = nil;
+  //_webAuthenticationVC = nil;
   _session = nil;
   _externalUserAgentFlowInProgress = NO;
+}
+
+#pragma mark - SFSafariViewControllerDelegate
+
+-(void)webViewDidFinish:(NSURL *)callbackURL
+{
+    __weak OIDExternalUserAgentCatalyst *weakSelf = self;
+
+    __strong OIDExternalUserAgentCatalyst *strongSelf = weakSelf;
+    if (!strongSelf) {
+        return;
+    }
+    //strongSelf->_webAuthenticationVC = nil;
+    if (callbackURL) {
+      [strongSelf->_session resumeExternalUserAgentFlowWithURL:callbackURL];
+    } else {
+      NSError *safariError =
+          [OIDErrorUtilities errorWithCode:OIDErrorCodeUserCanceledAuthorizationFlow
+                           underlyingError:nil
+                               description:@"Unexpected Error"];
+      [strongSelf->_session failExternalUserAgentFlowWithError:safariError];
+    }
+}
+
+-(void)webViewDidClose
+{
+      __weak OIDExternalUserAgentCatalyst *weakSelf = self;
+
+      __strong OIDExternalUserAgentCatalyst *strongSelf = weakSelf;
+      if (!strongSelf) {
+          return;
+      }
+      
+      NSError *safariError =
+          [OIDErrorUtilities errorWithCode:OIDErrorCodeUserCanceledAuthorizationFlow
+                           underlyingError:nil
+                               description:@"Unexpected Error"];
+
+      //NSLog(@"webViewDidClose: error: %@", error);
+      [strongSelf->_session failExternalUserAgentFlowWithError:safariError];
 }
 
 #pragma mark - ASWebAuthenticationPresentationContextProviding
